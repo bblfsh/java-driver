086a23656d438ce69148fac1812b1cac3f69a41d
Denys Smirnov <denys@sourced.tech>
2018-08-02 20:17:47 +0300

    support protocol v2
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- driver/fixtures/fixtures_test.go
+++ driver/fixtures/fixtures_test.go
@@ -5,8 +5,9 @@ import (
 	"testing"
 
 	"github.com/bblfsh/java-driver/driver/normalizer"
-	"gopkg.in/bblfsh/sdk.v2/sdk/driver"
-	"gopkg.in/bblfsh/sdk.v2/sdk/driver/fixtures"
+	"gopkg.in/bblfsh/sdk.v2/driver"
+	"gopkg.in/bblfsh/sdk.v2/driver/fixtures"
+	"gopkg.in/bblfsh/sdk.v2/driver/native"
 )
 
 const projectRoot = "../../"
@@ -15,8 +16,8 @@ var Suite = &fixtures.Suite{
 	Lang: "java",
 	Ext:  ".java",
 	Path: filepath.Join(projectRoot, fixtures.Dir),
-	NewDriver: func() driver.BaseDriver {
-		return driver.NewExecDriverAt(filepath.Join(projectRoot, "build/bin/native"))
+	NewDriver: func() driver.Native {
+		return native.NewDriverAt(filepath.Join(projectRoot, "build/bin/native"), native.UTF8)
 	},
 	Transforms: normalizer.Transforms,
 	BenchName:  "wildcard_type", // TODO: find a really large java file
