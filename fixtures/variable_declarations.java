c279e2ffe5aced36d0a5ba3059e058e239020d12
Denys Smirnov <denys@sourced.tech>
2018-10-17 18:21:22 +0300

    add roles for primitive type; fix #83
    Signed-off-by: Denys Smirnov <denys@sourced.tech>

--- fixtures/variable_declarations.java
+++ fixtures/variable_declarations.java
@@ -1,11 +1,12 @@
 class Code {
 	void code() {
 	    int a;
-	    int b = 1;
-	    int c, d;
-	    int e = 1, f;
-	    int g, h = 1;
-	    int i = 1, j = 1;
-	    int k = b = 1;
+	    byte b = 1;
+	    char c, d;
+	    short e = 1, f;
+	    long g, h = 1;
+	    float i = 1, j = 1;
+	    double k = b = 1;
+	    boolean x = true;
 	}
 }
