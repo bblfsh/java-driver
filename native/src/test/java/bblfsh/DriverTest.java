5d152c157fd5f0d0b1370ca961418fd75f285464
Alexander Bezzubov <bzz@apache.org>
2019-02-27 18:30:45 +0100

    native: update test fixtures
    Signed-off-by: Alexander Bezzubov <bzz@apache.org>

--- native/src/test/java/bblfsh/DriverTest.java
+++ native/src/test/java/bblfsh/DriverTest.java
@@ -67,7 +67,7 @@ public class DriverTest {
         driver.processOne();
 
         final String result = new String(out.toByteArray());
-        assertThat(result).isEqualTo("{\"status\":\"fatal\",\"errors\":[\"Unrecognized token 'garbage': was expecting ('true', 'false' or 'null')\\n at [Source: garbage; line: 1, column: 15]\"]}\n");
+        assertThat(result).isEqualTo("{\"status\":\"fatal\",\"errors\":[\"Unrecognized token 'garbage': was expecting ('true', 'false' or 'null')\\n at [Source: (String)\\\"garbage\\\"; line: 1, column: 15]\"]}\n");
     }
 
     @Test
